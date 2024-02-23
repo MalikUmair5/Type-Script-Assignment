8d11c3dd3a0443b3c51737dd10cf191a405f8d8f
MalikUmair5 <malikumair.dev@gmail.com>
2024-02-23 10:53:05 +0500

    Question 7 Completed

---
+++ Question 7/script.tsx
@@ -0,0 +1,21 @@
+let additon = 4 + 4;
+let subtraction = 10 - 2;
+let multiplication = 4 * 2;
+let division = 16 / 2;
+
+let additonElement = document.getElementById("add");
+let multiplicationElement = document.getElementById("mult");
+let subtractionElement = document.getElementById("sub");
+let divisionElement = document.getElementById("div");
+
+if (
+  additonElement &&
+  multiplicationElement &&
+  divisionElement &&
+  subtractionElement
+) {
+  additonElement.innerText = `Addition: ${additon}`;
+  multiplicationElement.innerText = `Multiplication ${multiplication}`;
+  subtractionElement.innerText = `Subtraction ${subtraction}`;
+  divisionElement.innerText = `Division ${division}`;
+}
