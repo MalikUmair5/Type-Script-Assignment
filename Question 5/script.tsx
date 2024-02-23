1a7576f72ec899639c538a832f49405791e8ec7c
MalikUmair5 <malikumair.dev@gmail.com>
2024-02-23 07:24:00 +0500

    Questin 5 Completed

---
+++ Question 5/script.tsx
@@ -0,0 +1,9 @@
+let message =
+  "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking";
+let famous_person = "Steve Jobs";
+
+let quoteElementq5 = document.getElementById("quote");
+
+if (quoteElementq5) {
+  quoteElementq5.innerHTML = `${famous_person} once said, "${message}."`;
+}
