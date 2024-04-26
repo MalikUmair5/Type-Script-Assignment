3c0ea82dc5fecd274b859629bbbd81ad5c5a1b24
“[Malik <[malikumair.dev@gmail.com]>
2024-04-25 22:58:27 -0700

    Question 13 Completed

---
+++ Question 13/app.js
@@ -0,0 +1,7 @@
+var Question13 = function () {
+    var names = ["Bike", "Car", "Bus", "Train", "Plane"];
+    names.forEach(function (names) {
+        console.log("I like to travel in ".concat(names));
+    });
+};
+Question13();
---
+++ Question 13/app.ts
@@ -0,0 +1,9 @@
+let Question13 = () => {
+    let names = ["Bike", "Car", "Bus", "Train", "Plane"];
+  
+    names.forEach(names => {
+        console.log(`I like to travel in ${names}`);
+    })
+  };
+  
+  Question13();
