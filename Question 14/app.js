de90d76954c47e7e743925fb4d85857dc696ea77
“[Malik <[malikumair.dev@gmail.com]>
2024-04-25 23:10:26 -0700

    Question 14 Completed

---
+++ Question 14/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let Question14 = () => {
+    let names = ["Abdullah", "Hassan", "Usaif", "Umair", "Hassan"];
+    names.forEach(names => {
+        console.log(`Dear ${names} I invite you to Dinner in my House Please Come`);
+    });
+};
+Question14();
---
+++ Question 14/app.ts
@@ -0,0 +1,9 @@
+let Question14 = () => {
+    let names = ["Abdullah", "Hassan", "Usaif", "Umair", "Hassan"];
+  
+    names.forEach(names => {
+        console.log(`Dear ${names} I invite you to Dinner in my House Please Come`);
+    })
+  };
+  
+  Question14();
