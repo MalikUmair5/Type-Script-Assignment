1c99b84f2a4eda448f8dd2d72207c1e84f98a1e6
“[Malik <[malikumair.dev@gmail.com]>
2024-05-08 13:44:57 -0700

    Question 20 Completed

---
+++ Question 20/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let Question19 = () => {
+    let languages = ["urdu", "english", "arabic", "chinese", "spanish", "punjabi", "sindhi", "pashto", "balochi", "kashmiri"];
+    console.log(languages);
+};
+Question19();
---
+++ Question 20/app.ts
@@ -0,0 +1,8 @@
+
+
+let Question19 = () => {
+    let languages : string[] = ["urdu", "english", "arabic", "chinese", "spanish", "punjabi", "sindhi", "pashto", "balochi", "kashmiri"];
+    console.log(languages)
+};
+
+Question19();
