acb8fec5b2e2e79da5939ac23de24d10137c8c21
“[Malik <[malikumair.dev@gmail.com]>
2024-05-09 14:18:19 -0700

    Quetion 29 Completed

---
+++ Question 29/app.js
@@ -0,0 +1,25 @@
+"use strict";
+let Question29 = () => {
+    let favourit_fruits = ["apple", "banana", "mango", "orange", "cheeko"];
+    favourit_fruits.find((fruit) => {
+        if (fruit == "mango") {
+            console.log("I really like mango");
+        }
+        else if (fruit == "banana") {
+            console.log("I really like banana");
+        }
+        else if (fruit == "orange") {
+            console.log("I really like orange");
+        }
+        else if (fruit == "apple") {
+            console.log("I really like apple");
+        }
+        else if (fruit == "cheeko") {
+            console.log("I really like cheeko");
+        }
+        else {
+            console.log("I really like nothing");
+        }
+    });
+};
+Question29();
