bf048d8ceb42f5b6f332dc6118b4d9f385597709
“[Malik <[malikumair.dev@gmail.com]>
2024-05-09 13:51:46 -0700

    Question 25 Completed

---
+++ Question 25/app.js
@@ -0,0 +1,11 @@
+"use strict";
+let Question25 = () => {
+    let alien_color = "green";
+    if (alien_color === "green") {
+        console.log("You just earned 5 points");
+    }
+    else {
+        // if condition fails this will run
+    }
+};
+Question25();
