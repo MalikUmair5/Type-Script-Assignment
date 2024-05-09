a9f126337029157ee608efd910d823e4e2241abd
“[Malik <[malikumair.dev@gmail.com]>
2024-05-09 13:59:51 -0700

    Question 27 Completed

---
+++ Question 27/app.ts
@@ -0,0 +1,21 @@
+
+
+let Question27 = () => {
+
+    let alien_color = "red";
+
+    if (alien_color === "green") {
+        console.log("Color is green, You just earned 5 points");
+    } 
+    else if (alien_color === "red") {
+        console.log("Color is red, You just earned 10 points");
+    }
+    else if(alien_color === "yellow"){
+        console.log("Color is yellow, You just earned 15 points");
+    }
+    else{
+        console.log("You just earned 0 Points");
+    }
+};
+
+Question27();
