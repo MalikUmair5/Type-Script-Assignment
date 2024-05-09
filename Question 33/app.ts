1784898c62ef4231c2f9c66fb397ed42815cbe55
“[Malik <[malikumair.dev@gmail.com]>
2024-05-09 14:41:22 -0700

    Question 33 Completed:

---
+++ Question 33/app.ts
@@ -0,0 +1,24 @@
+
+
+let Question33 = () => {
+
+    let number : number[] = [1,2,3,4,5,6,7,8,9];
+
+    for (let i = 0; i < number.length; i++) {
+        if (number[i] === 1) {
+            console.log("1st");
+        }
+        else if (number[i] === 2) {
+            console.log("2nd");
+        }
+        else if (number[i] === 3) {
+            console.log("3rd");
+        }
+        else{
+            console.log(`${number[i]}th`);
+        }
+    }
+
+};
+
+Question33();
