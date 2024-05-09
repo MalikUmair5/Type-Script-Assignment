fbf2fe83605aedcf05c0a3bb8d965c2da1be9975
“[Malik <[malikumair.dev@gmail.com]>
2024-05-09 13:55:12 -0700

    Question 26 Completed

---
+++ Question 26/app.js
@@ -0,0 +1,14 @@
+"use strict";
+let Question26 = () => {
+    let alien_color = "red";
+    if (alien_color === "green") {
+        console.log("You just earned 5 points");
+    }
+    else if (alien_color != "green") {
+        console.log("You just earned 10 points");
+    }
+    else {
+        // if all conditon fail this this will run
+    }
+};
+Question26();
---
+++ Question 26/app.ts
@@ -0,0 +1,19 @@
+
+
+let Question26 = () => {
+
+    let alien_color = "red";
+
+    if (alien_color === "green") {
+        console.log("You just earned 5 points");
+    } 
+    else if (alien_color != "green") {
+        console.log("You just earned 10 points");
+    }
+    else{
+        // if all conditon fail this this will run
+    }
+
+};
+
+Question26();
