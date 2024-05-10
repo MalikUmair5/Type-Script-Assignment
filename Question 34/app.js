d5813e451aec298a138b070a31e8ad72524e50a9
“[Malik <[malikumair.dev@gmail.com]>
2024-05-09 21:39:00 -0700

    Question 34 Completed

---
+++ Question 34/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let Question34 = () => {
+    let favourit_pizzas = ["Chicken Fajita", "Chicken Tikka", "Chicken BBQ", "Chicken Cheese", "Chicken Supreme", "Chicken Golden", "Chicken Malai", "Chicken Chapli", "Chicken Afghani", "Chicken Behari"];
+    for (let i = 0; i < favourit_pizzas.length; i++) {
+        console.log(`i Like ${favourit_pizzas[i]} Pizza`);
+    }
+};
+Question34();
---
+++ Question 34/app.ts
@@ -0,0 +1,12 @@
+
+
+let Question34 = () => {
+    let favourit_pizzas: string[] = ["Chicken Fajita", "Chicken Tikka", "Chicken BBQ", "Chicken Cheese", "Chicken Supreme", "Chicken Golden", "Chicken Malai", "Chicken Chapli", "Chicken Afghani", "Chicken Behari"];
+
+    for (let i = 0; i < favourit_pizzas.length; i++) {
+        console.log(`i Like ${favourit_pizzas[i]} Pizza`);
+    }
+
+};
+
+Question34();
