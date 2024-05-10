5f1674d1ab0fb33fd71fb8378eecf5680d04f45f
“[Malik <[malikumair.dev@gmail.com]>
2024-05-09 21:44:14 -0700

    Question 35 completed

---
+++ Question 35/app.js
@@ -0,0 +1,9 @@
+"use strict";
+let Question35 = () => {
+    let common_animals = ["dog", "cat", "fish", "lizard", "bird", "hamster", "gerbil", "turtle", "rabbit", "snake"];
+    for (let i = 0; i < common_animals.length; i++) {
+        console.log(`A ${common_animals[i]} would make a great pet`);
+    }
+    console.log("all these animls are common pets in the world");
+};
+Question35();
---
+++ Question 35/app.ts
@@ -0,0 +1,14 @@
+
+
+let Question35 = () => {
+    let common_animals: string[] = ["dog", "cat", "fish", "bird", "turtle", "rabbit"];
+
+    for(let i = 0; i < common_animals.length; i++){
+        console.log(`A ${common_animals[i]} would make a great pet`);
+    }
+
+    console.log("all these animls are common pets in the world")
+
+};
+
+Question35();
