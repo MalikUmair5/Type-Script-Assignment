a3878b1aabcdb5edcfd560ab34f64dea1401c611
“[Malik <[malikumair.dev@gmail.com]>
2024-05-09 21:48:16 -0700

    Question 36 Completed

---
+++ Question 36/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let Question36 = () => {
+    let make_tshirt = (size, text_messege) => {
+        console.log(`Tshirt is ready with size ${size} and text message ${text_messege}`);
+    };
+    make_tshirt(144, "Hello I love Pakistan");
+};
+Question36();
---
+++ Question 36/app.ts
@@ -0,0 +1,13 @@
+
+
+let Question36 = () => {
+
+    let make_tshirt = (size: number, text_messege: string) => {
+        console.log(`Tshirt is ready with size ${size} and text message ${text_messege}`);
+    }
+
+    make_tshirt(144, "Hello I love Pakistan");
+
+};
+
+Question36();
